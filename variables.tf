e7e0526411b8114ed34add2021e806d989853cc6
gurdeepsingh49 <deep.singh1014@gmail.com>
2025-07-18 16:29:08 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy resources"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket"
+}
